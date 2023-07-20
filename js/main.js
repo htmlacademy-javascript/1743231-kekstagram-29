9746db1953335d8d1393f0387390c30aeb87a7b8
AlbinaRaf <albinaabbasova09@gmail.com>
2023-07-20 23:31:09 +0300

    Добавим сообщение об ошибке, если не удалось получить данные с сервера

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 import initGallery from './gallery.js';
+import renderStatus from './status.js';
 import './upload.js';
 import { request } from './utils.js';
 
+try {
 /**
  * @type {Array<Picture>}
  */
-const data = await request('https://29.javascript.pages.academy/kekstagram/data');
+  const data = await request('https://29.javascript.pages.academy/kekstagram/data');
 
-initGallery(data);
+  initGallery(data);
+} catch (error) {
+  const title = `Ошибка: ${error.message}`;
+  const button = 'Закрыть';
+
+  renderStatus('error', {title, button});
+}
